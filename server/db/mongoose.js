e487754f422950e57217e255a42a13ab062bfadf
yash <yashojha19@gmail.com>
2018-03-30 23:44:12 +0530

    post api

---
+++ server/db/mongoose.js
@@ -0,0 +1,39 @@
+var mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/ToDoApp');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to ToDoApp database.");
+});
+
+module.exports ={
+    mongoose
+}
+
+
+
+///------------------ new model -----------------------
+// var dogs = mongoose.Schema({
+//     name:String,
+//     speak:String,
+//     goodboy_amount:Number
+// })
+
+// var dogoModel = mongoose.model('DogsInTheHood',dogs);
+
+// var newDogo = new dogoModel({
+//     name:'Vicktor',
+//     speak:"Woof",
+//     goodboy_amount:10
+// })
+
+// newDogo.save().then(res => console.log("Woof Result:",res))
+// .catch((err)=>{
+//     console.log("Error saving data to dogs model. woof")
+// })
+/////-----------------------------------------------------
+
