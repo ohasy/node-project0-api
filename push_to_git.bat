83ca8436ebb7306cdb0f05d6659729171c878be6
yash <yashojha19@gmail.com>
2018-04-29 01:08:52 +0530

    pushing to git and heroku automated

--- push_to_git.bat
+++ push_to_git.bat
@@ -6,3 +6,13 @@ git add .
 SET /P commit_msg=Please enter your commit msg:
 git commit -m "%commit_msg%"
 git push -u origin master
+SET /P heroku=Do you want to push it to heroku also? <y/n>:
+IF %heroku%=="y"||"yes" GOTO HEROKU_PUSH
+GOTO END
+:HEROKU_PUSH
+echo puhsing to heroku
+git push -u origin heroku
+echo puhed to heroku successfully.
+:END
+echo pushed to git successfully.
+pause
