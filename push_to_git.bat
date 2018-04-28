df7a17cb23e60523876b0dad3b0af076fcaf15be
yash <yashojha19@gmail.com>
2018-04-29 01:13:13 +0530

    push to git

--- push_to_git.bat
+++ push_to_git.bat
@@ -6,14 +6,14 @@ git add .
 SET /P commit_msg=Please enter your commit msg:
 git commit -m "%commit_msg%"
 git push -u origin master
+echo pushed to heroku successfully.
 pause
-SET /P push_heroku=Do you want to push it to heroku also:
-IF %push_heroku%=="y" GOTO HEROKU_PUSH
+SET /P push_heroku=Do you want to push it to heroku also y or n:
+IF %push_heroku%=='y' GOTO HEROKU_PUSH
 GOTO END
 :HEROKU_PUSH
 echo puhsing to heroku
 git push -u origin heroku
-echo puhed to heroku successfully.
 :END
 echo pushed to git successfully.
 pause
