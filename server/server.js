c3db0621ca47f60c96ce368464a96a587bd5953e
yash <yashojha19@gmail.com>
2018-04-29 15:24:06 +0530

    associate users with todos

--- server/server.js
+++ server/server.js
@@ -19,12 +19,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json())
  
 //post api for /todos route
-app.post('/todos',(req,res)=>{
+app.post('/todos',authenticate,(req,res)=>{
 
     console.log(req.body)
 
     var todo = new Todo({
-        text:req.body.text
+        text:req.body.text,
+        _creator:req.user._id
     })
 
     todo.save().then((doc)=>{
@@ -35,9 +36,11 @@ app.post('/todos',(req,res)=>{
     })
 })
 
-//making get api for /todos route to get all todos
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
+//making get api for /todos route to get all todos created by that user
+app.get('/todos',authenticate,(req,res)=>{
+    Todo.find({
+        _creator:req.user._id
+    }).then((todos)=>{
         res.send({todos})
     }).catch((e)=>{
         res.status(400).send(e);
@@ -45,11 +48,11 @@ app.get('/todos',(req,res)=>{
 })
 
 //making get api for /todos for perticular todo
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
     //res.send(req.params);
     let id = req.params.id
     if(ObjectID.isValid(id)){
-        Todo.findById(req.params.id).then((todo)=>{
+        Todo.findOne({_id:req.params.id,_creator:req.user._id}).then((todo)=>{
             if(!todo){
                 return  res.status(400).send({ERROR:"ID DOES'NT EXISTS"})
             }
@@ -66,13 +69,16 @@ app.get('/todos/:id',(req,res)=>{
 })
 
 //making an api for /todos to delete any todo
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id',authenticate,(req,res)=>{
     //get the id
     let id = req.params.id
     if(!ObjectID.isValid(id)){
         return res.status(404).send("Error: id is not in correct format")
-    }
-        Todo.findByIdAndRemove(id).then((todo)=>{
+    }   
+        Todo.findOneAndRemove({
+            _id:id,
+            _creator:req.user._id
+        }).then((todo)=>{
            if(!todo){
                return res.status(404).send("Error: there is no todo for this id.")
            }
@@ -85,7 +91,7 @@ app.delete('/todos/:id',(req,res)=>{
 })
 
 //making an api for /todos to update any todo item.
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
     let id = req.params.id;
     let body = _.pick(req.body,['text','completed'])
 
@@ -100,8 +106,8 @@ app.patch('/todos/:id',(req,res)=>{
         body.completedAt = null; 
     }
 
-    Todo.findByIdAndUpdate(
-        id,{$set:body},{new:true}).then((todo)=>{ //new true means it should return the updated item not the original one. 
+    Todo.findOneAndUpdate(
+        {_id:id,_creator:req.user._id},{$set:body},{new:true}).then((todo)=>{ //new true means it should return the updated item not the original one. 
             if(!todo){
                 return res.status(404).send('todo item not found.')
             }
